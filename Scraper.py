4d5c74a118430d1a6eab906c7104f34e0dc9eb00
Michael Apers <32524673+perfectclear@users.noreply.github.com>
2018-03-24 22:27:21 -0400

    Add files via upload

--- Scraper.py
+++ Scraper.py
@@ -1,76 +1,100 @@
-from ScraperUtils import ScrapeUserId, ScrapeMatchList, ScrapeMatchData
+from ScraperUtils import scrape_user_id, scrape_match_list, scrape_match_data
 import csv
+from urllib.error import HTTPError
+from time import sleep
+import re
 
-#seed_name = 'JPFog'
 
-def CollectDataFromSeedPlayer(seed_name):
-    seed_id = ScrapeUserId(seed_name)
-    name_id_set = set((seed_name, seed_id))
-    match_set = ScrapeMatchList(seed_id)
-    with open("PUBG_match_ids.csv", 'a') as f:
-        f.write(str(match_set) + "," + "\n")
-    used_names = set(seed_name)
-    with open("PUBG_used_names.csv", 'a') as f:
-        f.write(str(seed_name) + "," + "\n")
+# seed_name = 'JPFog'
+
+
+def collect_data_from_seed_player(seed_name):
+    seed_id = scrape_user_id(seed_name)
+    name_id_set = {(seed_name, seed_id)}
+    match_set = scrape_match_list(seed_id)
+    with open("PUBG_match_ids.tsv", 'a') as f:
+        f.write(re.sub('[\s+]', '', str(match_set)) + "\t" + "\n")
+    used_names = {seed_name}
+    with open("PUBG_used_names.tsv", 'a') as f:
+        f.write(re.sub('[\s+]', '', str(used_names)) + "\t" + "\n")
     used_matches = set()
-    match_data = set()
     counter = 0
     for match_id in match_set:
-        name_ids_from_match, data_from_match = ScrapeMatchData(match_id)
-        name_id_set.add(name_ids_from_match)
-        with open("PUBG_name_ids.csv", 'a') as f:
-            f.write(str(name_ids_from_match) + "," + "\n")
-        match_data.add(data_from_match)
-        with open("PUBG_MatchData.csv", 'a') as f:
-            f.write(str(data_from_match) + "," + "\n")
-        used_matches.add(match_id)
-        with open("PUBG_used_match_ids.csv", 'a') as f:
-            f.write(str(match_id) + "," + "\n")
-        counter = counter+1
-        print(counter)
+        while True:
+            try:
+                name_ids_from_match, data_from_match = scrape_match_data(match_id)
+                new_name_ids_from_match = name_ids_from_match - name_id_set
+                with open("PUBG_name_ids.tsv", 'a') as f:
+                    f.write(re.sub('[\s+]', '', str(new_name_ids_from_match)))
+                name_id_set.update(name_ids_from_match)
+                with open("PUBG_MatchData.tsv", 'a') as f:
+                    f.write(re.sub('[\s+]', '', str(data_from_match)) + "\t" + "\n")
+                used_matches.add(match_id)
+                with open("PUBG_used_match_ids.tsv", 'a') as f:
+                    f.write(re.sub('[\s+]', '', str(match_id)))
+                counter = counter+1
+                print(counter)
+                break
+            except HTTPError:
+                try:
+                    print('Ratelimited!')
+                    sleep(10)
+                    name_ids_from_match, data_from_match = scrape_match_data(match_id)
+                    new_name_ids_from_match = name_ids_from_match - name_id_set
+                    with open("PUBG_name_ids.tsv", 'a') as f:
+                        f.write(re.sub('[\s+]', '', str(new_name_ids_from_match)))
+                    name_id_set.update(name_ids_from_match)
+                    with open("PUBG_MatchData.tsv", 'a') as f:
+                        f.write(re.sub('[\s+]', '', str(data_from_match)) + "\t" + "\n")
+                    used_matches.add(match_id)
+                    with open("PUBG_used_match_ids.tsv", 'a') as f:
+                        f.write(re.sub('[\s+]', '', str(match_id)))
+                    counter = counter+1
+                    print(counter)
+                    continue
+                except HTTPError:
+                    print('Double HTTPError! SKIPPING!')
+                    break
 
 
-def CollectDataExpandFromFile():
+def collect_data_expand_from_file():
 
     counter = 1
 
-    with open('PUBG_name_ids.csv', 'r') as f:
+    with open('PUBG_name_ids.tsv', 'r') as f:
         reader = csv.reader(f)
         name_id_set = set(reader)
-    with open('PUBG_used_names.csv', 'r') as f:
+    with open('PUBG_used_names.tsv', 'r') as f:
         reader = csv.reader(f)
         used_names = set(reader)
-    with open('PUBG_match_ids.csv', 'r') as f:
+    with open('PUBG_match_ids.tsv', 'r') as f:
         reader = csv.reader(f)
         match_set = set(reader)
-    with open('PUBG_used_match_ids.csv', 'r') as f:
+    with open('PUBG_used_match_ids.tsv', 'r') as f:
         reader = csv.reader(f)
         used_matches = set(reader)
-    with open('PUBG_MatchData.csv', 'r') as f:
-        reader = csv.reader(f)
-        match_data = set(reader)
 
-    for (name,id) in name_id_set:
+    for (name, player_id) in name_id_set:
         if name not in used_names:
-            match_ids = ScrapeMatchList(id)
-            match_set.add(match_ids)
-            with open("PUBG_match_ids.csv", 'a') as f:
-                f.write(str(match_set) + "," + "\n")
+            match_ids = scrape_match_list(player_id)
+            new_match_ids = match_ids - match_set
+            match_set.update(match_ids)
+            with open("PUBG_match_ids.tsv", 'a') as f:
+                f.write(re.sub('[\s+]', '', str(new_match_ids)))
             used_names.add(name)
-            with open("PUBG_used_names.csv", 'a') as f:
-                f.write(str(name) + "," + "\n")
+            with open("PUBG_used_names.tsv", 'a') as f:
+                f.write(re.sub('[\s+]', '', str(name)) + "\t" + "\n")
             for match_id in match_ids:
                 if match_id not in used_matches:
-                    name_ids_from_match, data_from_match = ScrapeMatchData(match_id)
-                    name_id_set.add(name_ids_from_match)
-                    with open("PUBG_name_ids.csv", 'a') as f:
-                        f.write(str(name_ids_from_match) + "," + "\n")
-                    match_data.add(data_from_match)
-                    with open("PUBG_MatchData.csv", 'a') as f:
-                        f.write(str(data_from_match) + "," + "\n")
+                    name_ids_from_match, data_from_match = scrape_match_data(match_id)
+                    new_name_ids_from_match = name_ids_from_match
+                    name_id_set.update(name_ids_from_match)
+                    with open("PUBG_name_ids.tsv", 'a') as f:
+                        f.write(re.sub('[\s+]', '', str(new_name_ids_from_match)))
+                    with open("PUBG_MatchData.tsv", 'a') as f:
+                        f.write(re.sub('[\s+]', '', str(data_from_match)) + "\t" + "\n")
                     used_matches.add(match_id)
-                    with open("PUBG_used_match_ids.csv", 'a') as f:
-                        f.write(str(match_id) + "," + "\n")
+                    with open("PUBG_used_match_ids.tsv", 'a') as f:
+                        f.write(re.sub('[\s+]', '', str(match_id)))
                     counter = counter+1
                     print(counter)
-
--- ScraperUtils.py
+++ ScraperUtils.py
@@ -5,42 +5,48 @@ Created on Thu Mar 22 20:50:13 2018
 @author: Michael
 """
 
-import pandas as pd
 from lxml import html
 import requests
 from urllib import request
 import json
 from time import sleep
+from contextlib import closing
 
 
-def ScrapeUserID(name):
+def scrape_user_id(name):
     page = requests.get('https://pubg.op.gg/user/{}?server=na'.format(name))
     tree = html.fromstring(page.content)
     user_id = tree.xpath('/descendant::div[@data-p-user_id]')[0].attrib['data-p-user_id']
     return user_id
 
-def ScrapeMatchList(user_id):
+
+def scrape_match_list(user_id):
     offset = ''
     match_ids = set()
     n = 0
     while n < 5:
         if offset == '':
-            with request.urlopen('https://pubg.op.gg/api/users/{}/matches/recent?server=na&queue_size=4&mode=fpp'.format(user_id,offset)) as url:
+            with closing(request.urlopen(
+                    'https://pubg.op.gg/api/users/{}/matches/recent?server=na&queue_size=4&mode=fpp'
+                    .format(user_id, offset))) as url:
                 data = json.loads(url.read().decode())
         else:
-            with request.urlopen('https://pubg.op.gg/api/users/{}/matches/recent?server=na&queue_size=4&mode=fpp&after={}'.format(user_id, offset)) as url:
+            with closing(request.urlopen(
+                    'https://pubg.op.gg/api/users/{}/matches/recent?server=na&queue_size=4&mode=fpp&after={}'
+                    .format(user_id, offset))) as url:
                 data = json.loads(url.read().decode())
         for j in range(0, len(data['matches']['items'])):
             offset = data['matches']['items'][j]['offset']
             match_id = data['matches']['items'][j]['match_id']
             match_ids.add(match_id)
-        sleep(2)
+        sleep(0.5)
         n = n + 1
     return match_ids
 
-def ScrapeMatchData(match_id):
+
+def scrape_match_data(match_id):
     name_id_set = set()
-    with request.urlopen('https://pubg.op.gg/api/matches/{}/deaths'.format(match_id)) as url:
+    with closing(request.urlopen('https://pubg.op.gg/api/matches/{}/deaths'.format(match_id))) as url:
         data = json.loads(url.read().decode())
     for j in range(0, len(data['deaths'])):
         victim_id = data['deaths'][j]['victim']['participant_id']
@@ -49,7 +55,6 @@ def ScrapeMatchData(match_id):
         if data['deaths'][j]['killer']:
             killer_name = data['deaths'][j]['killer']['user']['nickname']
             killer_id = data['deaths'][j]['killer']['participant_id']
-            name_id_set.add((killer_name,killer_id))
-    sleep(2)
+            name_id_set.add((killer_name, killer_id))
+    sleep(0.5)
     return name_id_set, data
-
